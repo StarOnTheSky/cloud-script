a81a24219621aa78655ad4f865024aea549530cb
StarOnTheSky <i@sots.eu.org>
2023-11-16 10:19:23 +0800

    fix home

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-wget https://github.com/staronthesky/cloud-script/raw/main/.zshrc -O /home/ubuntu/.zshrc
-wget https://github.com/staronthesky/cloud-script/raw/main/.p10k.zsh -O /home/ubuntu/.p10k.zsh
-git clone https://github.com/ohmyzsh/ohmyzsh.git /home/ubuntu/.oh-my-zsh
-git clone https://github.com/romkatv/powerlevel10k.git /home/ubuntu/.oh-my-zsh/themes/powerlevel10k
+if [ ! $HOME ] ; then
+    HOME=/home/ubuntu
+fi
+wget https://github.com/staronthesky/cloud-script/raw/main/.zshrc -O $HOME/.zshrc
+wget https://github.com/staronthesky/cloud-script/raw/main/.p10k.zsh -O $HOME/.p10k.zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
+git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/themes/powerlevel10k
 chsh -s /bin/zsh ubuntu
