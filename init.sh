52dcbd220f79bfc8fb4f8a229ede3025ee138a37
StarOnTheSky <i@sots.eu.org>
2023-11-16 10:23:33 +0800

    fix user

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ ! $HOME ] ; then
     HOME=/home/ubuntu
+    USER=ubuntu
 fi
 wget https://github.com/staronthesky/cloud-script/raw/main/.zshrc -O $HOME/.zshrc
 wget https://github.com/staronthesky/cloud-script/raw/main/.p10k.zsh -O $HOME/.p10k.zsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/themes/powerlevel10k
-chsh -s /bin/zsh ubuntu
+chsh -s /bin/zsh $USER
